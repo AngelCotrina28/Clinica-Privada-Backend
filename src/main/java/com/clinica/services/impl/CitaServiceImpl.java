5e7f7747fe18787d7a6d832247b893af29223e72
AngelCotrina28 <cotrinaruizangeleduardo@gmail.com>
2026-05-16 16:46:05 -0500

    Avance... 2

--- src/main/java/com/clinica/services/impl/CitaServiceImpl.java
+++ src/main/java/com/clinica/services/impl/CitaServiceImpl.java
@@ -33,7 +33,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
