6633ebd56c40694f935a6bd5631cd806c57a70bb
AngelCotrina28 <cotrinaruizangeleduardo@gmail.com>
2026-06-21 19:14:44 -0500

    Error en DetalleOrdenEntregaRepository

--- src/main/java/com/clinica/model/repositories/DetalleOrdenEntregaRepository.java
+++ src/main/java/com/clinica/model/repositories/DetalleOrdenEntregaRepository.java
@@ -2,8 +2,6 @@ package com.clinica.model.repositories;
 
 import com.clinica.model.entities.DetalleOrdenEntrega;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DetalleOrdenEntregaRepository extends JpaRepository<DetalleOrdenEntrega, Long> {
 }
